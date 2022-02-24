c59903a326c5d68e912eac69f5ada8ef859fb244
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-24 12:40:43 +0700

    refactor

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Common",
-            targets: ["Common"]),
+            targets: ["Common"]
+            )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
