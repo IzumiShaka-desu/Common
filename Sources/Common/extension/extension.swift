19f5ce5802b0df55179816f6fe3e4c77031bb486
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-24 15:22:34 +0700

    fixed error

--- Sources/Common/extension/extension.swift
+++ Sources/Common/extension/extension.swift
@@ -4,22 +4,7 @@
 //
 //  Created by Akashaka on 24/02/22.
 //
-
-import Foundation
 import SwiftUI
-extension UIColor {
-    static let flatDarkBackground = UIColor(red: 36, green: 36, blue: 36)
-    static let flatDarkCardBackground = UIColor(red: 46, green: 46, blue: 46)
-    static let flatWhiteBackground = UIColor(red: 255, green: 255, blue: 255, alpha: 0.3)
-
-    convenience init(red: Int,
-                     green: Int,
-                     blue: Int,
-                     alpha: CGFloat = 1.0) {
-        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha)
-    }
-}
-
 extension Color {
     public init(decimalRed red: Double, green: Double, blue: Double) {
         self.init(red: red / 255, green: green / 255, blue: blue / 255)
