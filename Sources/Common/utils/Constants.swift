1e54d47193cd3d39757f1985e2bd59e667d3aa26
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-24 19:42:26 +0700

    set to public

--- Sources/Common/utils/Constants.swift
+++ Sources/Common/utils/Constants.swift
@@ -7,19 +7,19 @@
 
 import SwiftUI
 
-class Constants {
-  static let profileImageUrl = "https://media.discordapp.net/"
+public class Constants {
+public  static let profileImageUrl = "https://media.discordapp.net/"
   + "attachments/916984491363409930/"
   + "940466191166763048/2021042706272366ec78fe14a65eb49c3476fae33ec75b.png"
 
-  static let profileName="Sesaka Aji Nursyah Bantani"
-  static let positionName="Mobile Developer"
-  static let baseUrl="https://api.rawg.io/api"
-  static let apiKey="989d0c911352450dbb8d51a2e9db86cc"
+public  static let profileName="Sesaka Aji Nursyah Bantani"
+public  static let positionName="Mobile Developer"
+public static let baseUrl="https://api.rawg.io/api"
+public static let apiKey="989d0c911352450dbb8d51a2e9db86cc"
 
-  static let radius: CGFloat = 16
-  static let indicatorHeight: CGFloat = 6
-  static let indicatorWidth: CGFloat = 60
-  static let snapRatio: CGFloat = 0.25
-  static let minHeightRatio: CGFloat = 0.01
+public static let radius: CGFloat = 16
+public  static let indicatorHeight: CGFloat = 6
+public  static let indicatorWidth: CGFloat = 60
+public  static let snapRatio: CGFloat = 0.25
+public  static let minHeightRatio: CGFloat = 0.01
 }
