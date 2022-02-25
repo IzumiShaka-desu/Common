cf9997afab2ee4a0f60af52d01d3e7bddf9599a5
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-25 07:30:19 +0700

    set public

--- Sources/Common/extension/extension.swift
+++ Sources/Common/extension/extension.swift
@@ -6,16 +6,16 @@
 //
 import SwiftUI
 public extension Color {
-    public init(decimalRed red: Double, green: Double, blue: Double) {
+     init(decimalRed red: Double, green: Double, blue: Double) {
         self.init(red: red / 255, green: green / 255, blue: blue / 255)
     }
 
-    public static var flatDarkBackground: Color {
+     static var flatDarkBackground: Color {
         return Color(decimalRed: 36, green: 36, blue: 36)
     }
-    public static var flatWhiteBackground: Color {Color(red: 255, green: 255, blue: 255, opacity: 0.3)}
+     static var flatWhiteBackground: Color {Color(red: 255, green: 255, blue: 255, opacity: 0.3)}
 
-    public static var flatDarkCardBackground: Color {
+     static var flatDarkCardBackground: Color {
         return Color(decimalRed: 46, green: 46, blue: 46)
     }
 }
